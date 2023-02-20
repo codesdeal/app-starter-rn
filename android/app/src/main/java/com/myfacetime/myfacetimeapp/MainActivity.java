9d22231375ed45ca3948d7baecea3fb839c8bd54
Abu Bakkar Siddique <46748540+codesdeal@users.noreply.github.com>
2023-02-20 21:25:49 +0600

    updated

--- android/app/src/main/java/com/myfacetime/myfacetimeapp/MainActivity.java
+++ android/app/src/main/java/com/myfacetime/myfacetimeapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rn_starter_kit;
+package com.myfacetime.myfacetimeapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "StarterApp";
+    return "MyFaceTime";
   }
 }
--- ios/rn_starter_kitTests/rn_starter_kitTests.m
+++ ios/rn_starter_kitTests/rn_starter_kitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rn_starter_kitTests : XCTestCase
+@interface MyFaceTimeTests : XCTestCase
 
 @end
 
-@implementation rn_starter_kitTests
+@implementation MyFaceTimeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
